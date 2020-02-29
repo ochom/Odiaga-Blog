acae9d6b64953f8c43dc7c692a75896e7b79c931
ochom <ochomrichard752@gmail.com>
2020-02-29 17:05:29 +0300

    1

---
+++ includes/head.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Site Metas -->
+    <title>Erickson Odiaga-Learn to code and earn</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    
+    <!-- Design fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet"> 
+
+    <!-- Bootstrap core CSS -->
+    <link href="statics/css/bootstrap.css" rel="stylesheet">
+
+    <!-- FontAwesome Icons core CSS -->
+    <link href="statics/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="statics/style.css" rel="stylesheet">
+
+    <!-- Responsive styles for this template -->
+    <link href="statics/css/responsive.css" rel="stylesheet">
+
+    <!-- Colors for this template -->
+    <link href="statics/css/colors.css" rel="stylesheet">
+
+    <!-- Version Garden CSS for this template -->
+    <link href="statics/css/version/garden.css" rel="stylesheet">
+</head>
---
+++ includes/scripts.php
@@ -0,0 +1,6 @@
+<!-- Core JavaScript
+================================================== -->
+<script src="statics/js/jquery.min.js"></script>
+<script src="statics/js/tether.min.js"></script>
+<script src="statics/js/bootstrap.min.js"></script>
+<script src="statics/js/custom.js"></script>
